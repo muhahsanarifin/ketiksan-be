631fdd60e84d31169c92b28cc075f105650de827
muhahsanarifin <muh.ahsan.student@gmail.com>
2024-01-28 08:08:14 +0700

    `Update files`, server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import fastify from "fastify";
-
 require("dotenv").config();
 const Fastify = require("fastify");
 const routes = require("./src/routes/index");
@@ -7,59 +5,35 @@ const cors = require("@fastify/cors");
 const pinoPretty = require("./src/helpers/logger");
 const { fastifyMailer } = require("./src/config/nodemailer");
 
-// const start = async () => {
-//   const fastify = Fastify({
-//     logger: pinoPretty,
-//   });
+const start = async () => {
+  const fastify = Fastify({
+    logger: pinoPretty,
+  });
 
-//   fastify.register(require("fastify-mailer"), {
-//     ...fastifyMailer({
-//       from: `${process.env.KETIKSAN_NODEMAILER_FROM}`,
-//       service: `${process.env.KETIKSAN_NODEMAILER_SERVICE}`,
-//       user: `${process.env.KETIKSAN_NODEMAILER_USER}`,
-//       pass: `${process.env.KETIKSAN_NODEMAILER_PASS}`,
-//     }),
-//   });
+  fastify.register(require("fastify-mailer"), {
+    ...fastifyMailer({
+      from: `${process.env.KETIKSAN_NODEMAILER_FROM}`,
+      service: `${process.env.KETIKSAN_NODEMAILER_SERVICE}`,
+      user: `${process.env.KETIKSAN_NODEMAILER_USER}`,
+      pass: `${process.env.KETIKSAN_NODEMAILER_PASS}`,
+    }),
+  });
 
-//   fastify.register(cors, {
-//     origin: process.env.BASE_URL,
-//   });
-//   fastify.register(require("@fastify/formbody"));
-//   fastify.register(routes);
+  fastify.register(cors, {
+    origin: process.env.BASE_URL,
+  });
+  fastify.register(require("@fastify/formbody"));
+  fastify.register(routes);
 
-//   try {
-//     const address = await fastify.listen({
-//       port: process.env.FASTIFY_PORT || 3000,
-//     });
-//     fastify.log.info(`server listening on ${address}`);
-//   } catch (err) {
-//     fastify.log.error(err);
-//     process.exit(1);
-//   }
-// };
-
-// start();
-
-const fastify = Fastify({
-  logger: pinoPretty,
-});
-
-fastify.register(require("fastify-mailer"), {
-  ...fastifyMailer({
-    from: `${process.env.KETIKSAN_NODEMAILER_FROM}`,
-    service: `${process.env.KETIKSAN_NODEMAILER_SERVICE}`,
-    user: `${process.env.KETIKSAN_NODEMAILER_USER}`,
-    pass: `${process.env.KETIKSAN_NODEMAILER_PASS}`,
-  }),
-});
-
-fastify.register(cors, {
-  origin: process.env.BASE_URL,
-});
-fastify.register(require("@fastify/formbody"));
-fastify.register(routes);
-
-export default async (req, res) => {
-  await fastify.ready();
-  fastify.server.emit("request", req, res);
+  try {
+    const address = await fastify.listen({
+      port: process.env.FASTIFY_PORT || 3000,
+    });
+    fastify.log.info(`server listening on ${address}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
+
+start();
